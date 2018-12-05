4a9fcf3e03f410fdfdc7f8f0460febd9ad476edc
minhchen <minhchen@wesure.cn>
2018-12-05 11:06:03 +0800

    add .gitattributes

---
+++ architecture/api-gateway.go
@@ -0,0 +1,14 @@
+## 网关笔记
+
+### 开源api网关
+
+- tyk(`https://tyk.io/docs/get-started/with-tyk-on-premise/`)
+- krakend(`http://www.krakend.io/`)
+
+### 路由模块
+
+- httprouter(`radix tree`)
+- mux(`数组`)
+- goji(`radix tree`)
+
+> _tips: 如果没有路由匹配需求可以考虑基于hashMap实现_
